b5195fbb76122839448d9aafe67d74d06e4b73f8
fcu-d0410328 <watch6145@gmail.com>
2019-02-21 14:45:19 +0800

    程式碼

---
+++ src/helloworld/hello.java
@@ -0,0 +1,20 @@
+package helloworld;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("helloworld");
+		
+		int v=2;
+		
+		double k=3.5;
+		
+		double z=v+k;
+		
+		System.out.println(z);
+		
+
+	}
+
+}
